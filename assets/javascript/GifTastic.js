9c3325c5957a7399efe32e99af6edd1d8ad00c2b
Jorge Salas <jorge_salas@Jorges-MBP.attlocal.net>
2017-10-23 19:32:46 -0700

    ready

--- assets/javascript/GifTastic.js
+++ assets/javascript/GifTastic.js
@@ -1,19 +1,72 @@
-var search = "chicken";
-var topic =[];
 
-for (var i = 0; i < topic.length; i++) {
-  $("")
+var topic =["mlb", "golden state warriors", "michael jordan", "los angeles dodgers", "aaron rodgers", "boston celtics", "gordon hayward", "new york yankees", "nba", "football"];
+
+// Display first set of buttons
+function displayButtons() {
+  $("#buttons").empty();
+  for (var i = 0; i < topic.length; i++) {
+    var gifButton = $("<button>");
+    gifButton.addClass("btn btn-primary gif-btn");
+    gifButton.text(topic[i]);
+
+    $("#buttons").append(gifButton);
+  }
 }
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=b4H9jZJL8ilOBzfKXEwUDjItsnyYMWdZ&q=" + search + "&limit=1&offset=0&rating=G&lang=en";
+// Add more buttons !
+$("#submit").on("click", function() {
+  topic.push($(".form-control").val());
+  displayButtons();
+});
 
-$.ajax({
-  url: queryURL,
-  method: 'GET'
-  }).done(function(response) {
 
-    var im = response.data['0'].images.fixed_height.url;
+// Make GIFs animate
+$(document).on("click", ".image", function() {
 
-    $("#gifs").append("<img src='" + im + "'>");
-    console.log(response);
+  var state = $(this).attr("data-state");
+
+  if(state === "animate") {
+    $(this).attr("src", $(this).attr("data-still"))
+    $(this).attr("data-state", "still");
+  } else {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  }
+});
+
+// Display GIFs to HTML
+$(document).on("click", ".gif-btn", function() {
+
+  $("#gifs").empty();
+
+  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=b4H9jZJL8ilOBzfKXEwUDjItsnyYMWdZ&q=" + $(this)["0"].textContent + "&limit=10&offset=0&rating=G&lang=en";
+
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+    }).done(function(response) {
+
+      var result = response.data;
+
+      for(var i = 0; i < result.length; i++) {
+
+        var gif = $("<div>");
+        gif.addClass("gif-box");
+        gif.append("Rating: " + result[i].rating.toUpperCase() + "<br>");
+
+        var image = $("<img>");
+        image.addClass("image");
+        image.attr("src", result[i].images.fixed_height_still.url);
+        image.attr("data-still", result[i].images.fixed_height_still.url);
+        image.attr("data-animate", result[i].images.fixed_height.url);
+        image.attr("data-state", "still");
+
+        gif.append(image);
+        $("#gifs").append(gif);
+
+      }
   });
+});
+
+displayButtons();
+
